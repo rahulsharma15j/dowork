fc2634c1a108cf45edf71440463b92133ce77d6a
Rahul Sharma <rahulsharma15j@gmail.com>
2021-08-19 22:25:22 +0530

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
 import "./App.scss";
-
+import Navbar from "./Components/Navbar/Navbar.js";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
